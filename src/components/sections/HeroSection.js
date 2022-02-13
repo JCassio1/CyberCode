655f7beede970914ce4963ff449ed2598fe18bf6
JCassio1 <Joselsondias@hotmail.com>
2022-02-13 22:33:17 +0000

    Make Content adaptive with margins and padding on Hero

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -23,4 +23,8 @@ export default HeroSection
 const Wrapper = styled.div`
   background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
 `
-const ContentWrapper = styled.div``
+const ContentWrapper = styled.div`
+  max-width: 1234px;
+  margin: 0 auto; /* Left and Right use auto*/
+  padding: 200px 30px;
+`
