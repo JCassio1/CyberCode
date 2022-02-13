beb91b4ac76e7131023a864b3965d17e5c353e65
JCassio1 <Joselsondias@hotmail.com>
2022-02-13 20:39:13 +0000

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Código QR`,
+    description: `Don't skip security. Come and learn how to make your websites secure and aesthetically pleasing.`,
+    author: `@JoselsonDias`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
